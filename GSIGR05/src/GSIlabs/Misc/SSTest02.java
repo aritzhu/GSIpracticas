8d554f192334b331847319b6b5e798e7cc075a16
mellado149322 <mellado.149322@e.unavarra.es>
2025-10-17 10:52:09 +0200

    ejercicos 1 y 2

---
+++ GSIGR05/src/GSIlabs/Misc/SSTest02.java
@@ -0,0 +1,48 @@
+package GSIlabs.Misc;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
+import javax.swing.table.DefaultTableModel;
+import org.jopendocument.dom.spreadsheet.SpreadSheet;
+import org.jopendocument.dom.spreadsheet.Sheet;
+
+public class SSTest02 {
+
+    public static void main(String[] args) {
+        int filas = 4;
+        int columnas = 6;
+        int[][] matriz = new int[filas][columnas];
+        Random rnd = new Random();
+
+        // Rellenar la matriz con datos aleatorios
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                matriz[i][j] = rnd.nextInt(10);
+            }
+        }
+
+        try {
+            // Crear una hoja con 10 filas, 10 columnas y 1 hoja
+            SpreadSheet hoja = SpreadSheet.create(1, 10, 10);
+            Sheet sheet = hoja.getSheet(0);
+
+            // Insertar los datos de la matriz en la hoja
+            for (int i = 0; i < filas; i++) {
+                for (int j = 0; j < columnas; j++) {
+                    sheet.setValueAt(matriz[i][j], i, j);
+                }
+            }
+
+            // Guardar el archivo ODS
+            File archivoODS = new File("test02.ods");
+            hoja.saveAs(archivoODS);
+
+            System.out.println("Archivo 'test02.ods' creado correctamente.");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Error al guardar el archivo ODS.");
+        }
+    }
+}
