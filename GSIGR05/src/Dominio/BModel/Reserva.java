469434a63337dce6b38429b886cf9c5787cce98f
Joritz <echenique.149324@e.unavarra.es>
2025-11-10 20:12:55 +0100

    Añadido xml para reserva

--- GSIGR05/src/Dominio/BModel/Reserva.java
+++ GSIGR05/src/Dominio/BModel/Reserva.java
@@ -5,13 +5,18 @@
 package Dominio.BModel;
 
 import Dominio.IBModelo.Local;
+import GSILabs.Serializable.XMLRepresentable;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
  *
  * @author alumno
  */
-public class Reserva {
+public class Reserva implements XMLRepresentable{
     private Date fechaReserva;
     private int descuento;
     private Cliente cliente;
@@ -57,6 +62,62 @@ public class Reserva {
 
     public void setLocal(Local local) {
         this.local = local;
+    }
+    
+    @Override
+    public String toXML() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        StringBuilder xml = new StringBuilder();
+
+        xml.append("<Reserva>\n");
+        xml.append("    <fechaReserva>").append(sdf.format(fechaReserva)).append("</fechaReserva>\n");
+        xml.append("    <descuento>").append(descuento).append("</descuento>\n\n");
+
+        if (cliente != null) {
+            xml.append("    <cliente>\n");
+            xml.append("        <id>").append(cliente.getID()).append("</id>\n");
+            xml.append("        <nick>").append(cliente.getNick()).append("</nick>\n");
+            xml.append("        <contrasenia>").append(cliente.getContrasenia()).append("</contrasenia>\n");
+            xml.append("        <edad>").append(cliente.getEdad()).append("</edad>\n");
+            xml.append("        <fechaNacimiento>")
+               .append(sdf.format(cliente.getFechaNacimiento()))
+               .append("</fechaNacimiento>\n");
+            xml.append("    </cliente>\n\n");
+        }
+
+        if (local != null) {
+            xml.append("    <local>\n");
+            xml.append("        <nombre>").append(local.getNombre()).append("</nombre>\n");
+
+            if (local.getDireccion() != null) {
+                xml.append("        <direccion>\n");
+                xml.append("            <localidad>").append(local.getDireccion().getLocalidad()).append("</localidad>\n");
+                xml.append("            <provincia>").append(local.getDireccion().getProvincia()).append("</provincia>\n");
+                xml.append("            <calle>").append(local.getDireccion().getCalle()).append("</calle>\n");
+                xml.append("            <numero>").append(local.getDireccion().getNumero()).append("</numero>\n");
+                xml.append("        </direccion>\n");
+            }
+
+            xml.append("    </local>\n");
+        }
+
+        xml.append("</Reserva>");
+        return xml.toString();
+    }
+    
+        @Override
+    public boolean saveToXML(File f) {
+        try (FileWriter fw = new FileWriter(f)) {
+            fw.write(this.toXML());
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean saveToXML(String filePath) {
+        return saveToXML(new File(filePath));
     }
     
     
